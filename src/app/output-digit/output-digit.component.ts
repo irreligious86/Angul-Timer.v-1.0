ea8c97e2ee2d7b46cde37f7462b1db71cec0596d
Alexxs <irreligious86@gmail.com>
2021-11-15 00:06:53 +0100

    create layout tree and set styles

---
+++ src/app/output-digit/output-digit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-output-digit',
+  templateUrl: './output-digit.component.html',
+  styleUrls: ['./output-digit.component.css']
+})
+export class OutputDigitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
